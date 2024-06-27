7516e2ac29072fc437ce19209222d0b172206866
Sanket Kumar Padhan <152056082+Sanket3yoProgrammer@users.noreply.github.com>
2024-06-27 16:34:13 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="mb-1 text-6xl font-extrabold text-blue-500">Yo Chat</h1>
-      <p className="mb-10">The coolest chat app on the planet</p>
+      <p className="mb-10">The coolest chat app on the planet for purple hand gang</p>
       <Button as={Link} href="/chat">
         Start chatting
       </Button>
